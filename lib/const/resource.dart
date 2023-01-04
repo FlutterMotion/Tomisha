3898c7d91adc408bfba055a18c6e347bd431caad
Umesh Shrestha <mihai.profiroiu@omnisourcetech.com>
2023-01-04 09:25:01 -0500

    tab bar section

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -4,7 +4,41 @@
 class R {
   const R._();
 
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/arrow_left.svg)
+  static const String ASSETS_SVG_ARROW_LEFT_SVG = 'assets/svg/arrow_left.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/arrow_right.svg)
+  static const String ASSETS_SVG_ARROW_RIGHT_SVG = 'assets/svg/arrow_right.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_Profile_data_re_v81r.svg)
+  static const String ASSETS_SVG_UNDRAW_PROFILE_DATA_RE_V81R_SVG =
+      'assets/svg/undraw_Profile_data_re_v81r.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_about_me_wa29.svg)
+  static const String ASSETS_SVG_UNDRAW_ABOUT_ME_WA29_SVG =
+      'assets/svg/undraw_about_me_wa29.svg';
+
   /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_agreement_aajr.svg)
   static const String ASSETS_SVG_UNDRAW_AGREEMENT_AAJR_SVG =
       'assets/svg/undraw_agreement_aajr.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_business_deal_cpi9.svg)
+  static const String ASSETS_SVG_UNDRAW_BUSINESS_DEAL_CPI9_SVG =
+      'assets/svg/undraw_business_deal_cpi9.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_job_offers_kw5d.svg)
+  static const String ASSETS_SVG_UNDRAW_JOB_OFFERS_KW5D_SVG =
+      'assets/svg/undraw_job_offers_kw5d.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_personal_file_222m.svg)
+  static const String ASSETS_SVG_UNDRAW_PERSONAL_FILE_222M_SVG =
+      'assets/svg/undraw_personal_file_222m.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_swipe_profiles1_i6mr.svg)
+  static const String ASSETS_SVG_UNDRAW_SWIPE_PROFILES1_I6MR_SVG =
+      'assets/svg/undraw_swipe_profiles1_i6mr.svg';
+
+  /// ![preview](file:///Users/umeshshrestha/Documents/self/prototype/assets/svg/undraw_task_31wc.svg)
+  static const String ASSETS_SVG_UNDRAW_TASK_31WC_SVG =
+      'assets/svg/undraw_task_31wc.svg';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,6 +23,13 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.latoTextTheme(),
         primaryColor: AppColors.lightColor.primary,
         primaryColorLight: AppColors.lightColor.primary,
+        tabBarTheme: TabBarTheme(
+          labelColor: AppColors.lightColor.white,
+          unselectedLabelColor: AppColors.lightColor.primary,
+          indicator: BoxDecoration(
+            color: AppColors.lightColor.primary,
+          ),
+        ),
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
             foregroundColor: AppColors.lightColor.primary1,
@@ -53,12 +61,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   late final ScrollController scrollController;
+  late final TabController tabController;
   @override
   void initState() {
     // TODO: implement initState
     scrollController = ScrollController();
+    tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
 
@@ -66,9 +77,69 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     // TODO: implement dispose
     scrollController.dispose();
+    tabController.dispose();
     super.dispose();
   }
 
+  final TabData tab0 = const TabData(
+    title: "Drei einfache Schritte zur Vermittlung a asd",
+    sections: [
+      TabSection(
+        title: "Erstellen dein Lebenslauf asdas das asd ad ",
+        asset: R.ASSETS_SVG_UNDRAW_PROFILE_DATA_RE_V81R_SVG,
+      ),
+      TabSection(
+        title: "Erstellen dein Lebenslauf as dasd asd asd asd",
+        asset: R.ASSETS_SVG_UNDRAW_TASK_31WC_SVG,
+      ),
+      TabSection(
+        title: "Mit nur einem Klick bewerben asd asd  asd s",
+        asset: R.ASSETS_SVG_UNDRAW_PERSONAL_FILE_222M_SVG,
+      ),
+    ],
+  );
+
+  final TabData tab1 = const TabData(
+    title: "Drei einfache Schritte zu deinem neuen Job",
+    sections: [
+      TabSection(
+        title: "Erstellen dein Lebenslauf",
+        asset: R.ASSETS_SVG_UNDRAW_PROFILE_DATA_RE_V81R_SVG,
+      ),
+      TabSection(
+        title: "Erstellen dein Lebenslauf",
+        asset: R.ASSETS_SVG_UNDRAW_TASK_31WC_SVG,
+      ),
+      TabSection(
+        title: "Mit nur einem Klick bewerben",
+        asset: R.ASSETS_SVG_UNDRAW_PERSONAL_FILE_222M_SVG,
+      ),
+    ],
+  );
+
+  final TabData tab2 = const TabData(
+    title: "Drei einfache Schritte zur Vermittlung neuer ",
+    sections: [
+      TabSection(
+        title: "Erstellen dein Lebenslauf asdas das ",
+        asset: R.ASSETS_SVG_UNDRAW_PROFILE_DATA_RE_V81R_SVG,
+      ),
+      TabSection(
+        title: "Erstellen dein Lebenslauf as dasd asd",
+        asset: R.ASSETS_SVG_UNDRAW_TASK_31WC_SVG,
+      ),
+      TabSection(
+        title: "Mit nur einem Klick bewerben asd asd ",
+        asset: R.ASSETS_SVG_UNDRAW_PERSONAL_FILE_222M_SVG,
+      ),
+    ],
+  );
+  int selectedIndex = 0;
+  List<String> tabs = [
+    "Arbeitnehmer",
+    "Arbeitgeber",
+    "Temporärbüro",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,11 +151,67 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         controller: scrollController,
         child: Column(
-          children: const [
-            RegisterComponent(),
-            SizedBox(height: 1003),
-            SizedBox(height: 1003),
-            SizedBox(height: 1003),
+          children: [
+            const RegisterComponent(),
+            const SizedBox(height: 50),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...tabs.mapIndexed(
+                      (index, element) => InkWell(
+                        onTap: () {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: index == 0
+                                  ? const Radius.circular(10)
+                                  : const Radius.circular(0),
+                              topRight: index == 2
+                                  ? const Radius.circular(10)
+                                  : const Radius.circular(0),
+                              bottomLeft: index == 0
+                                  ? const Radius.circular(10)
+                                  : const Radius.circular(0),
+                              bottomRight: index == 2
+                                  ? const Radius.circular(10)
+                                  : const Radius.circular(0),
+                            ),
+                            color: selectedIndex == index
+                                ? AppColors.lightColor.primary
+                                : null,
+                            border: Border.all(
+                              color: selectedIndex == index
+                                  ? AppColors.lightColor.primary
+                                  : AppColors.lightColor.borderColor,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            "Arbeitnehmer",
+                            style: GoogleFonts.lato().copyWith(
+                                color: selectedIndex == index
+                                    ? Colors.white
+                                    : AppColors.lightColor.primary),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            const SizedBox(height: 50),
+            TabView(
+              data: selectedIndex == 0
+                  ? tab0
+                  : selectedIndex == 1
+                      ? tab1
+                      : tab2,
+            ),
           ],
         ),
       ),
@@ -227,7 +354,7 @@ class RegisterComponent extends StatelessWidget {
                           color: AppColors.themedColor(context).boxShadow,
                           blurRadius: 9,
                           spreadRadius: 0,
-                          offset: Offset(0, -8)),
+                          offset: const Offset(0, -8)),
                     ],
                     borderRadius: const BorderRadius.only(
                       topLeft: Radius.circular(12),
@@ -255,6 +382,228 @@ class RegisterComponent extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class TabData {
+  final String title;
+  final List<TabSection> sections;
+
+  const TabData({
+    required this.title,
+    required this.sections,
+  });
+}
+
+class TabSection {
+  final String title;
+  final String asset;
+
+  const TabSection({
+    required this.title,
+    required this.asset,
+  });
+}
+
+class TabView extends StatelessWidget {
+  final TabData data;
+  const TabView({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Container(
+          constraints: const BoxConstraints(maxWidth: 350),
+          child: Text(
+            data.title,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        Stack(
+          children: [
+            Column(
+              children: [
+                ...data.sections.mapIndexed(
+                  (i, e) {
+                    final index = i + 1;
+                    return LayoutBuilder(builder: (context, c) {
+                      return Column(
+                        children: [
+                          if (index != 2)
+                            SizedBox(height: screenWidth > 760 ? 100 : 40)
+                          else
+                            SizedBox(height: screenWidth > 760 ? 150 : 60),
+                          ClipPath(
+                            // clipper: index.isEven ? TabSectionClipper() : null,
+                            child: Container(
+                              width: double.infinity,
+                              // height: index.isEven ? 400 : null,
+                              decoration: index.isEven
+                                  ? const BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          Color(0xffE6FFFA),
+                                          Color(0xffEBF4FF),
+                                        ],
+                                      ),
+                                    )
+                                  : null,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  if (index.isEven) ...[
+                                    _buildImage(screenWidth, e),
+                                    const SizedBox(width: 70),
+                                  ],
+                                  SizedBox(
+                                    width: screenWidth > 760
+                                        ? 500
+                                        : screenWidth * .5,
+                                    child: Stack(
+                                      clipBehavior: Clip.none,
+                                      children: [
+                                        SizedBox(
+                                          height: 260,
+                                          width: 385,
+                                          child: CircleAvatar(
+                                            radius: 80,
+                                            backgroundColor: i.isEven
+                                                ? AppColors
+                                                    .lightColor.backgroundColor
+                                                : Colors.transparent,
+                                          ),
+                                        ),
+                                        Positioned(
+                                          bottom: (screenWidth > 760
+                                                  ? 260
+                                                  : screenWidth * .2) /
+                                              2,
+                                          left: (screenWidth > 760
+                                                  ? 260
+                                                  : screenWidth * .2) /
+                                              2,
+                                          child: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                "$index.",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline2
+                                                    ?.copyWith(
+                                                      color:
+                                                          AppColors.themedColor(
+                                                                  context)
+                                                              .textColor1,
+                                                      height: .5,
+                                                    ),
+                                              ),
+                                              const SizedBox(width: 10),
+                                              Flexible(
+                                                child: Text(
+                                                  e.title,
+                                                  maxLines: 2,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .headline6
+                                                      ?.copyWith(
+                                                          color: AppColors
+                                                                  .themedColor(
+                                                                      context)
+                                                              .textColor1),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  if (index.isOdd) ...[
+                                    const SizedBox(width: 70),
+                                    _buildImage(screenWidth, e),
+                                  ]
+                                ],
+                              ),
+                            ),
+                          ),
+                          if (index != 2)
+                            SizedBox(height: screenWidth > 760 ? 100 : 40)
+                          else
+                            SizedBox(height: screenWidth > 760 ? 50 : 20)
+                        ],
+                      );
+                    });
+                  },
+                ),
+              ],
+            ),
+            if (screenWidth > 760)
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 300,
+                child: SvgPicture.asset(
+                  R.ASSETS_SVG_ARROW_RIGHT_SVG,
+                ),
+              ),
+            if (screenWidth > 760)
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 300,
+                child: SvgPicture.asset(
+                  R.ASSETS_SVG_ARROW_LEFT_SVG,
+                ),
+              ),
+          ],
+        )
+      ],
+    );
+  }
+
+  SizedBox _buildImage(double screenWidth, TabSection e) {
+    return SizedBox(
+      height: screenWidth > 970 ? 260 : screenWidth * .3,
+      width: screenWidth > 970 ? 385 : screenWidth * .3,
+      child: SvgPicture.asset(
+        e.asset,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
+
+class TabSectionClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var firstStart = Offset(size.width / 5, size.height);
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    var secondEnd = Offset(size.width, size.height - 80);
+
+    final path = Path()
+      ..lineTo(0.0, size.height)
+      ..quadraticBezierTo(
+          firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy)
+      ..quadraticBezierTo(
+          secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy)
+      ..lineTo(size.width, 0.0)
+      ..close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
 
