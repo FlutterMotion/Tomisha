9b8e10599ddbbdca3764f3434428d9cb953e3458
Umesh Shrestha <mihai.profiroiu@omnisourcetech.com>
2023-01-05 03:12:47 -0500

    fix flexible

--- lib/main.dart
+++ lib/main.dart
@@ -511,19 +511,17 @@ class TabView extends StatelessWidget {
                                                   width: screenWidth > 760
                                                       ? 400
                                                       : 300,
-                                                  child: Flexible(
-                                                    child: Text(
-                                                      e.title,
-                                                      maxLines: 2,
-                                                      style: Theme.of(context)
-                                                          .textTheme
-                                                          .headline6
-                                                          ?.copyWith(
-                                                              color: AppColors
-                                                                      .themedColor(
-                                                                          context)
-                                                                  .textColor1),
-                                                    ),
+                                                  child: Text(
+                                                    e.title,
+                                                    maxLines: 2,
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .headline6
+                                                        ?.copyWith(
+                                                            color: AppColors
+                                                                    .themedColor(
+                                                                        context)
+                                                                .textColor1),
                                                   ),
                                                 ),
                                               ],
